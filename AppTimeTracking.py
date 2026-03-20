b8456064807c9de1812644d2c0c56d83b0328758
Tsek63 <tsek@live.be>
2026-03-20 18:57:56 +0100

    Update AppTimeTracking.py

--- AppTimeTracking.py
+++ AppTimeTracking.py
@@ -105,14 +105,15 @@ def run(conn):
                     
                     if st.session_state.get(f"confirm_{i}"):
                         st.warning("Supprimer ?")
-                        if st.button("OUI ✅", key=f"yes_{i}"):
-                            df_updated = df.drop(i)
-                            try:
-                                conn.update(worksheet="TimeTracking", data=df_updated)
-                                del st.session_state[f"confirm_{i}"] # Nettoyage
-                                st.rerun()
-                            except Exception as e_del:
-                                st.error(f"Erreur : {e_del}")
+                       if st.button("OUI ✅", key=f"yes_{i}"):
+    df_updated = df.drop(i)
+    try:
+        conn.update(worksheet="TimeTracking", data=df_updated)
+        st.cache_data.clear()     # 🔥 IMPORTANT : recharge propre
+        del st.session_state[f"confirm_{i}"]
+        st.rerun()
+    except Exception as e_del:
+        st.error(f"Erreur : {e_del}")
                         if st.button("NON ❌", key=f"no_{i}"):
                             del st.session_state[f"confirm_{i}"]
                             st.rerun()
